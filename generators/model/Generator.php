d20799ac6eec8e35e99247b0df26b1f1b5462db1
Yohanes Candrajaya <mootensai@users.noreply.github.com>
2016-06-14 09:33:15 +0700

    support fk column with front id_
    Support foreign key column like id_user
    not only user_id.

--- generators/model/Generator.php
+++ generators/model/Generator.php
@@ -634,6 +634,9 @@ class Generator extends \yii\gii\Generator
         if (!empty($key) && substr_compare($key, 'id', -2, 2, true) === 0 && strcasecmp($key, 'id')) {
             $key = rtrim(substr($key, 0, -2), '_');
         }
+        if(!empty($key) && substr_compare($key, 'id', 0, 2, true) === 0 && strcasecmp($key, 'id')){
+            $key = ltrim(substr($key, 2, strlen($key)), '_');
+        }
         if ($multiple) {
             $key = Inflector::pluralize($key);
         }
